0695898ffcfd19096441bccb9969eee8c11e8d8a
Cyber108705 <Cyber108705@gmail.com>
2022-01-23 13:03:40 +0000

    add

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from winreg import REG_QWORD
 from flask import Flask,request,render_template
 
 app = Flask(__name__)
@@ -6,9 +8,13 @@ app = Flask(__name__)
 def hello_world():
 	return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-	return render_template('signup.html')
+	if(request.method=='POST'):
+		user = request.form['user']
+		passwd = request.form['passwd']
+		
+		return render_template('signup.html')
 
 if(__name__==('__main__')):
 	app.run(debug=True)
